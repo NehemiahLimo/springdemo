2c88a58eaa0e919a3eee7e316cae888470ef8375
NehemiahLimo <nehemiahlimo02@gmail.com>
2022-03-01 21:42:08 +0300

    Completed docker setup, postgres

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,12 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao{
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +24,28 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(),"Person from db"));
-    }
+        final String sql = "SELECT id,name FROM person";
+        return jdbcTemplate.query(sql,(resultset,i)->{
+            return new Person(UUID.fromString(resultset.getString("id")),resultset.getString("name"));
+        });
+        //return people;
 
+//        return List.of(new Person(UUID.randomUUID(),"Person from db"));
+    }
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id,name FROM person WHERE id= ?";
+
+        Person person = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{id},
+                (resultset, i)->{
+                    UUID personId = UUID.fromString(resultset.getString("id"));
+                    String name = resultset.getString("name");
+                    return new Person(personId,name);
+                });
+        return Optional.ofNullable(person);
+
     }
 
     @Override
